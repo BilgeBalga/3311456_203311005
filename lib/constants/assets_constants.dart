1eb24fe2a0a80b49218eaadbe859b59a6300fff3
Bilge Balga <82833137+BilgeBalga@users.noreply.github.com>
2023-06-21 16:46:07 +0300

    final

--- lib/constants/assets_constants.dart
+++
@@ -1,7 +0,0 @@
-class AssetPathConstants {
-  final String loginAssetPath = "assets/backgr.jpg";
-  final String popularImage =
-      'https://images.unsplash.com/photo-1617195920950-1145bf9a9c72?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YXVkaSUyMGNhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60';
-  final String recommendationImage =
-      "https://cdn2.rcstatic.com/images/car_images/new_images/peugeot/2008_lrg.jpg";
-}
